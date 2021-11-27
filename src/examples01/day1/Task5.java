17a573c2a80e57713e83f0281c5d8e1485cd3cc8
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2021-11-27 12:57:44 +0300

    Exm01. day1. Task5

---
+++ src/examples01/day1/Task5.java
@@ -0,0 +1,18 @@
+package examples01.day1;
+
+/**
+ * @author Nikolay Pavlov
+ *
+ * Повторить задание 4, но используя цикл for
+ */
+
+public class Task5 {
+    public static void main(String[] args) {
+
+        int age = 23;
+
+        for (int i = age; i <= 100; i += 5) {
+            System.out.println("Мой возраст " + i);
+        }
+    }
+}
