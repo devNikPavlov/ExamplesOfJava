06721bb7e7e5c77b0374662ef61c55a24acab6c8
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2021-11-27 12:33:52 +0300

    Exm01. day1. Task3

---
+++ src/examples01/day1/Task3.java
@@ -0,0 +1,16 @@
+package examples01.day1;
+
+/**
+ * @author Nikolay Pavlov
+ *
+ * Вывести слово на экран "JAVA", в столбик, чтобы оно повторилось 10 раз, используя любой цикл
+ */
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("JAVA");
+        }
+    }
+}
