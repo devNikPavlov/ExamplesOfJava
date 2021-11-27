6fe213e327289153af97391174f32198eb53817b
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2021-11-27 12:29:48 +0300

    Exm01. day1. Task2

---
+++ src/examples01/day1/Task2.java
@@ -0,0 +1,16 @@
+package examples01.day1;
+
+/**
+ * "author Nikolay Pavlov
+ *
+ * Повторить задание 1б но используя цикл for
+ */
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Java ");
+        }
+    }
+}
