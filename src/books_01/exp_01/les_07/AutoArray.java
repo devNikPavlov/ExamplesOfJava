8dddcbf32d87b21e32a2a60f74057fc38880c71c
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-05-14 11:41:02 +0300

    Book 01, exp 01, les 07

---
+++ src/books_01/exp_01/les_07/AutoArray.java
@@ -0,0 +1,11 @@
+package books_01.exp_01.les_07;
+
+// Усовершенствованная версия предыдущей программы
+
+public class AutoArray {
+    public static void main(String[] args) {
+        int moyth_days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        System.out.println("В апреле " + moyth_days[3] + " дней.");
+    }
+}
